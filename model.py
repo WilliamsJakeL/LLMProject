6d9b0c0263f01b564e3e1fc0113f29bbaa12183b
WilliamsJakeL <williamsjl@uchicago.edu>
2023-11-14 14:43:39 -0600

    Create a function to get the logits for the next token in the sequence - useful for multiple choice

--- model.py
+++ model.py
@@ -334,7 +334,7 @@ class GPT(nn.Module):
         return mfu
 
     @torch.no_grad()
-    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
+    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None:
         """
         Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
@@ -359,3 +359,23 @@ class GPT(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
 
         return idx
+
+    @torch.no_grad()
+    def get_next_token_options(self, idx, temperature=1.0, top_k=None:
+        """
+        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and get
+        the logits for the single next token.
+        """
+        # if the sequence context is growing too long we must crop it at block_size
+        idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
+        # forward the model to get the logits for the index in the sequence
+        logits, _ = self(idx_cond)
+        # pluck the logits at the final step and scale by desired temperature
+        logits = logits[:, -1, :] / temperature
+        # optionally crop the logits to only the top k options
+        if top_k is not None:
+            v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+            logits[logits < v[:, [-1]]] = -float('Inf')
+        return logits
+        # apply softmax to convert logits to (normalized) probabilities
+        # probs = F.softmax(logits, dim=-1)
